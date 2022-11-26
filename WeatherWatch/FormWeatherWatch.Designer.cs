5037a75b4b742ce1b15e390424cd9d35840cbfc8
Richard McAlister <rick-mcalister@msn.com>
2022-11-26 06:34:04 -0800

    Add project files.

---
+++ WeatherWatch/FormWeatherWatch.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace WeatherWatch
+{
+    partial class FormWeatherWatch
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.WeatherReport = new System.Windows.Forms.TextBox();
+            this.CloseButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // WeatherReport
+            // 
+            this.WeatherReport.Location = new System.Drawing.Point(6, 12);
+            this.WeatherReport.Multiline = true;
+            this.WeatherReport.Name = "WeatherReport";
+            this.WeatherReport.ReadOnly = true;
+            this.WeatherReport.Size = new System.Drawing.Size(194, 269);
+            this.WeatherReport.TabIndex = 1;
+            // 
+            // CloseButton
+            // 
+            this.CloseButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.CloseButton.Location = new System.Drawing.Point(78, 288);
+            this.CloseButton.Name = "CloseButton";
+            this.CloseButton.Size = new System.Drawing.Size(59, 22);
+            this.CloseButton.TabIndex = 2;
+            this.CloseButton.Text = "Close";
+            this.CloseButton.UseVisualStyleBackColor = true;
+            this.CloseButton.Click += new System.EventHandler(this.CloseButton_Click);
+            // 
+            // FormWeatherWatch
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.CancelButton = this.CloseButton;
+            this.ClientSize = new System.Drawing.Size(212, 322);
+            this.Controls.Add(this.CloseButton);
+            this.Controls.Add(this.WeatherReport);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormWeatherWatch";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.WindowsDefaultBounds;
+            this.Text = "WeatherWatch 1.0";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox WeatherReport;
+        private System.Windows.Forms.Button CloseButton;
+    }
+}
+
---
+++ WeatherWatch/FormWeatherWatch.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Windows.Forms;
+using TheSkyXLib;
+
+namespace WeatherWatch
+{
+    public partial class FormWeatherWatch : Form
+    {
+        public bool openFlag;
+
+        public FormWeatherWatch()
+        {
+            InitializeComponent();
+            this.Show();
+            openFlag = true;
+
+            WeatherReport.Text = "Monitoring CloudWatcher";
+            this.Show();
+            System.Windows.Forms.Application.DoEvents();
+
+            WeatherFileReader wmon;
+
+            while (openFlag)
+            {
+                wmon = new WeatherFileReader();
+                WeatherReport.Text = wmon.WeatherDataListing();
+                for (int i = 0; i < 11; i++)
+                {
+                    System.Threading.Thread.Sleep(1000);
+                    System.Windows.Forms.Application.DoEvents();
+                    if (!openFlag) { Close(); }
+                    if (wmon.AlertFlag == WeatherFileReader.WeaAlert.Alert)
+                    {
+                        WeatherReport.Text = "Unsafe Weather Detected.  Closing Dome";
+                        this.Show();
+                        System.Windows.Forms.Application.DoEvents();
+                        sky6Dome tsxd = new sky6Dome();
+                        if (tsxd.IsConnected == 0)
+                        { tsxd.Connect(); }
+                        tsxd.CloseSlit();
+                        while (tsxd.IsCloseComplete == 0)
+                        {
+                            tsxd.CloseSlit();
+                            System.Threading.Thread.Sleep(10000);
+                        }
+                        tsxd = null;
+                        WeatherReport.Text = "Unsafe Weather Detected.  Dome Closed";
+                        this.Show();
+                        System.Windows.Forms.Application.DoEvents();
+                        break;
+                    }
+                }
+                wmon = null;
+            }
+            Close();
+            return;
+        }
+
+        private void CloseButton_Click(object sender, EventArgs e)
+        {
+            openFlag = false;
+            return;
+        }
+    }
+}
